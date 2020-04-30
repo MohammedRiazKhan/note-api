1c82cf233396b404fd2c04b46e2d1673c3607b35
Mohammed Riaz Khan <riaz_khan96@outlook.com>
2020-04-30 10:04:20 +0200

    Project Structure

--- angular-client/src/app/model/note/note.ts
+++
@@ -1,7 +0,0 @@
-export class Note {
-
-  noteId: number;
-  noteTitle: string;
-  noteContent: string;
-
-}
--- src/main/java/me/mohammedriazkhan/note/NoteApplication.java
+++ src/main/java/me/mohammedriazkhan/note/NoteApplication.java
@@ -3,7 +3,6 @@ package me.mohammedriazkhan.note;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class NoteApplication {
 
